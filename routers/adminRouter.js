3b01af2ba3e2d716e5fcec501254b257b67b920e
Shuai Wang <wsking233@gmail.com>
2024-06-25 14:42:06 +1200

    Update adminRouter.js

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -5,13 +5,13 @@ const pendingFreejoaController = require('../controllers/pendingFreejoaControlle
 
 
 // get all pending freejoas
-router.get('/pending/all', verifyToken, checkPermission([ADMIN]), pendingFreejoaController.getAllUploadRequest); //only admin can get all pending freejoas
+router.get('/pending/freejoas/all', verifyToken, checkPermission([ADMIN]), pendingFreejoaController.getAllUploadRequest); //only admin can get all pending freejoas
 
 // approve pending freejoas
-router.patch('/pending/approve', verifyToken, checkPermission([ADMIN]), pendingFreejoaController.approvePendingFreejoas); //only admin can approve pending freejoas
+router.patch('/pending/freejoas/approve', verifyToken, checkPermission([ADMIN]), pendingFreejoaController.approvePendingFreejoas); //only admin can approve pending freejoas
 
 // reject pending freejoas
-router.delete('/pending/reject', verifyToken, checkPermission([ADMIN]), pendingFreejoaController.rejectPendingFreejoas); //only admin can reject pending freejoas
+router.delete('/pending/freejoas/reject', verifyToken, checkPermission([ADMIN]), pendingFreejoaController.rejectPendingFreejoas); //only admin can reject pending freejoas
 
 
 module.exports = router;
